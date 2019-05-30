04cc6391c6a0a163952dcf262d1db245e96d3efe
Kenneth Solano <klsolano77@gmail.com>
2019-05-30 16:05:56 -0400

    s

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const baseURL = process.env.NODE_ENV === "production"
 ? "/api"
-: "https://thelinkup.herokuapp.com/api"; 
+: "/"; 
 
 const service = axios.create({
   baseURL:baseURL,
